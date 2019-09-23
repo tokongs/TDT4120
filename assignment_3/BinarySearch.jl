9ae7c1dde4f450529d143c640b6ec736d344d063
Tobias Kongsvik <tokongs98@gmail.com>
2019-09-23 16:06:56 +0200

    Rewrite Binary search to actually work :/

--- assignment_3/BinarySearch.jl
+++ assignment_3/BinarySearch.jl
@@ -1,58 +1,46 @@
 function binaryintervalsearch(x,delta, coordinate)
-    divisible_by_two = false
-    if size(x, 1) % 2 == 0
-        left = x[1:div(size(x, 1), 2)-1, :, :]
-        right = x[div(size(x, 1), 2)+2:end, :, :]
-        median = (x[div(size(x, 1), 2), coordinate, 1] + x[div(size(x, 1), 2)+1, coordinate, 1]) / 2
-    else
-        left = x[1:div(size(x, 1), 2), :, :]
-        right = x[div(size(x, 1), 2)+2:end, :, :]
+    minimum = maximum = -1
+    left_r = div(size(x, 1), 2)
+    left_l = 1
+    right_l = div(size(x, 1), 2)+1
+    right_r = size(x, 1)
+    median = (x[div(size(x, 1), 2), coordinate, 1] + x[div(size(x, 1), 2)+1, coordinate, 1]) / 2
+    if size(x, 1) % 2 != 0
+        minimum = maximum = div(size(x, 1), 2) + 1
         median = x[div(size(x, 1), 2)+1, coordinate, 1]
     end
 
-    maximum = -1
-    minimum = -1
-    l = 1
-    r = size(left, 1)
-    m = l + div(r-l, 2)
-    while l < r
-        m = l + div(r-l, 2)
-        if m == l
-            break
-        end
-        if median-delta > left[m, coordinate, 1]
-            l = m+1
+    while left_r - left_l > 1
+        middle = left_l + div(left_r - left_l, 2)
+        if median-delta <= x[middle, coordinate, 1]
+            left_r = middle
         else
-            r = m
-        end 
-    end
-    if left[r, coordinate, 1] >= median-delta
-        minimum = r
-    end
-
-    l = 1
-    r = size(right, 1)
-
-    while l < r
-        m = l + div(r-l, 2)
-        if m == l
-            break
+            left_l = middle
         end
-        if median+delta < right[m, coordinate, 1]
-            r = m-1
+    end
+    if x[left_l, coordinate, 1] >= median-delta
+        minimum = left_l
+    elseif x[left_r, coordinate, 1] >= median-delta
+        minimum = left_r
+    end
+
+    while right_r - right_l > 1
+        middle = right_l + div(right_r - right_l, 2)
+        if median-delta <= x[middle, coordinate, 1]
+            right_r = middle
         else
-            l = m
-        end 
+            right_l = middle
+        end
     end
-
-    if right[l, coordinate, 1] <= median+delta
-            maximum = div(size(x, 1), 2) + l
-            if size(x, 1) % 2 != 0
-                maximum += 1
-            end
+    if x[right_r, coordinate, 1] <= median+delta
+        maximum = right_r
+    elseif x[right_l, coordinate, 1] <= median+delta
+        maximum = right_l
     end
-
     return minimum, maximum
 end
 
-println(binaryintervalsearch( [1.0 0.0; 2.0 0.0; 3.0 0.0], 0.5,  1))
+println(binaryintervalsearch([1 2; 2 3; 3 0; 4 0; 5 1], 1.5, 1))
+println(binaryintervalsearch([1 2; 2 0; 3 3; 4 4], 0.25, 1))
+println(binaryintervalsearch([1 0; 2 0; 2 0; 3 0; 4 0; 5 0; 5 0], 1, 1))
+println(binaryintervalsearch([1 0; 2 0; 3 0], 0.25, 1))
